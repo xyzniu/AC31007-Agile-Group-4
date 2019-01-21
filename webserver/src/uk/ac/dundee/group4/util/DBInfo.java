263870bb8e312d2fd088c215ad6a56f8db4154af
xinyue niu <xinyue.z.niu@gmail.com>
2019-01-21 16:26:50 +0000

    Delete DBInfo.java

--- webserver/src/uk/ac/dundee/group4/util/DBInfo.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.dundee.group4.util;
-
-public class DBInfo {
-
-    public static final String url = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/18agileteam4db";
-    public static final String name = "18agileteam4";
-    public static final String password = "7632.at4.2367";
-}
